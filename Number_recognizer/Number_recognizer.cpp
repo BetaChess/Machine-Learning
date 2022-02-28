b40e436a51a9dd30678b78ab9a60154f3dc7e56e
William Jensen <williamjensenbc@gmail.com>
2022-02-28 19:47:11 +0100

    Initial Commit

---
+++ Number_recognizer/Number_recognizer.cpp
@@ -0,0 +1,12 @@
+﻿// Machine Learning.cpp : Defines the entry point for the application.
+//
+
+#include "Number_recognizer.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+
+	return 0;
+}
